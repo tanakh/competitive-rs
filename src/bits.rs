2433e1b4e30dfbc692ed5a17d4b8f93da183a5ee
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-01-29 05:26:51 +0900

    Rename

--- src/bits.rs
+++ src/bits.rs
@@ -1,11 +1,11 @@
 use std::ops::Index;
 
-pub struct BitSet(u32);
+pub struct SmallBitSet(u32);
 
 const TRUE: bool = true;
 const FALSE: bool = false;
 
-impl Index<usize> for BitSet {
+impl Index<usize> for SmallBitSet {
     type Output = bool;
     fn index(&self, index: usize) -> &bool {
         if self.0 & (1 << index) != 0 {
@@ -16,14 +16,14 @@ impl Index<usize> for BitSet {
     }
 }
 
-impl BitSet {
+impl SmallBitSet {
     pub fn count(&self) -> usize {
         self.0.count_ones() as usize
     }
 }
 
 /// Returns power of bitset (n <= 31)
-pub fn power_bitset(n: usize) -> impl Iterator<Item = BitSet> {
+pub fn power_bitset(n: usize) -> impl Iterator<Item = SmallBitSet> {
     assert!(n <= 31);
     PowerBitSetIter {
         cur: 0,
@@ -37,10 +37,10 @@ struct PowerBitSetIter {
 }
 
 impl Iterator for PowerBitSetIter {
-    type Item = BitSet;
-    fn next(&mut self) -> Option<BitSet> {
+    type Item = SmallBitSet;
+    fn next(&mut self) -> Option<SmallBitSet> {
         if self.cur < (1 << self.n) {
-            let ret = BitSet(self.cur);
+            let ret = SmallBitSet(self.cur);
             self.cur += 1;
             Some(ret)
         } else {
