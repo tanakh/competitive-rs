e6ee116efd174a406b99dd24cffe11ce1deefcaa
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-03-08 02:17:48 +0900

    use typenum for GF

--- src/gf.rs
+++ src/gf.rs
@@ -1,196 +1,225 @@
-/// Define a finite field for given prime
-#[macro_export]
-macro_rules! def_gf {
-    ($p:expr) => {
-        mod gf {
-            use std::convert::{From, Into, TryInto};
-            use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use crate::util::Echo;
+use std::convert::{From, Into, TryInto};
+use std::fmt::Display;
+use std::marker::PhantomData;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use typenum::Unsigned;
 
-            #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
-            pub struct GF(pub u64);
+/// Finite field of prime `P`
+///
+/// `P` must be an instance of `typenum::Unsigned` and a prime number.
+///
+/// ```
+/// use competitive::prelude::*;
+///
+/// type GF = competitive::gf::GF<promote!(1000000007)>;
+///
+/// let t = GF::new(2);
+/// assert_eq!(t.pow(100).as_u64(), 976371285);
+/// ```
+///
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct GF<P>(pub u64, PhantomData<P>);
 
-            pub const P: u64 = $p;
+impl<P> Clone for GF<P> {
+    fn clone(&self) -> Self {
+        Self(self.0, PhantomData::<P>)
+    }
+}
 
-            impl std::fmt::Display for GF {
-                fn fmt(&self, f: &mut ::std::fmt::Formatter) -> std::fmt::Result {
-                    write!(f, "{}", self.0)
-                }
+impl<P> Copy for GF<P> {}
+
+impl<P> Display for GF<P> {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<P> Echo for GF<P> {
+    fn echo(&self, w: &mut impl std::io::Write) -> Result<(), std::io::Error> {
+        write!(w, "{}", self.0)
+    }
+}
+
+impl<P: Unsigned> GF<P> {
+    pub fn new<T: TryInto<i64>>(v: T) -> Self {
+        Self(
+            v.try_into().ok().unwrap().rem_euclid(P::to_i64()) as u64,
+            PhantomData::<P>,
+        )
+    }
+
+    pub fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub fn pow(self, mut r: u64) -> Self {
+        let mut k = self;
+        let mut ret = GF::<P>::new(1);
+        while r > 0 {
+            if r % 2 != 0 {
+                ret = ret * k;
             }
+            r /= 2;
+            k = k * k;
+        }
+        ret
+    }
 
-            impl $crate::util::Echo for GF {
-                fn echo(&self, w: &mut impl std::io::Write) -> Result<(), std::io::Error> {
-                    write!(w, "{}", self.0)
-                }
-            }
+    // This requires P is prime
+    pub fn recip(self) -> Self {
+        self.pow(P::to_u64() - 2)
+    }
+}
 
-            impl GF {
-                pub fn new<T: TryInto<i64>>(v: T) -> GF {
-                    GF(v.try_into().ok().unwrap().rem_euclid(P as i64) as u64)
-                }
+impl<P: Unsigned, T: Into<GF<P>>> Add<T> for GF<P> {
+    type Output = Self;
+    fn add(self, rhs: T) -> Self::Output {
+        Self::new(self.0 + rhs.into().0)
+    }
+}
 
-                pub fn pow(self, mut r: u64) -> GF {
-                    let mut k = self;
-                    let mut ret = 1.into();
-                    while r > 0 {
-                        if r % 2 != 0 {
-                            ret = ret * k;
-                        }
-                        r /= 2;
-                        k = k * k;
-                    }
-                    ret
-                }
+impl<P: Unsigned, T: Into<GF<P>>> AddAssign<T> for GF<P> {
+    fn add_assign(&mut self, rhs: T) {
+        *self = *self + rhs;
+    }
+}
 
-                // This requires M is prime
-                pub fn recip(self) -> GF {
-                    self.pow(P - 2)
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> Sub<T> for GF<P> {
+    type Output = Self;
+    fn sub(self, rhs: T) -> Self::Output {
+        Self::new(self.0 + P::to_u64() - rhs.into().0)
+    }
+}
 
-            impl<T: Into<GF>> Add<T> for GF {
-                type Output = GF;
-                fn add(self, rhs: T) -> Self::Output {
-                    GF::new(self.0 + rhs.into().0)
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> SubAssign<T> for GF<P> {
+    fn sub_assign(&mut self, rhs: T) {
+        *self = *self - rhs;
+    }
+}
 
-            impl<T: Into<GF>> AddAssign<T> for GF {
-                fn add_assign(&mut self, rhs: T) {
-                    *self = *self + rhs;
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> Mul<T> for GF<P> {
+    type Output = Self;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::new(self.0 * rhs.into().0)
+    }
+}
 
-            impl<T: Into<GF>> Sub<T> for GF {
-                type Output = GF;
-                fn sub(self, rhs: T) -> Self::Output {
-                    GF::new(self.0 + P - rhs.into().0)
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> MulAssign<T> for GF<P> {
+    fn mul_assign(&mut self, rhs: T) {
+        *self = *self * rhs;
+    }
+}
 
-            impl<T: Into<GF>> SubAssign<T> for GF {
-                fn sub_assign(&mut self, rhs: T) {
-                    *self = *self - rhs;
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> Div<T> for GF<P> {
+    type Output = Self;
+    fn div(self, rhs: T) -> Self::Output {
+        self * rhs.into().recip()
+    }
+}
 
-            impl<T: Into<GF>> Mul<T> for GF {
-                type Output = GF;
-                fn mul(self, rhs: T) -> Self::Output {
-                    GF::new(self.0 * rhs.into().0)
-                }
-            }
+impl<P: Unsigned, T: Into<GF<P>>> DivAssign<T> for GF<P> {
+    fn div_assign(&mut self, rhs: T) {
+        *self = *self / rhs;
+    }
+}
 
-            impl<T: Into<GF>> MulAssign<T> for GF {
-                fn mul_assign(&mut self, rhs: T) {
-                    *self = *self * rhs;
-                }
-            }
-
-            impl<T: Into<GF>> Div<T> for GF {
-                type Output = GF;
-                fn div(self, rhs: T) -> Self::Output {
-                    self * rhs.into().recip()
-                }
-            }
-
-            impl<T: Into<GF>> DivAssign<T> for GF {
-                fn div_assign(&mut self, rhs: T) {
-                    *self = *self / rhs;
-                }
-            }
-
-            macro_rules! def_ops {
-                ($ty:ty) => {
-                    impl Add<GF> for $ty {
-                        type Output = GF;
-                        fn add(self, rhs: GF) -> Self::Output {
-                            GF::new(self) + rhs
-                        }
-                    }
-                    impl Sub<GF> for $ty {
-                        type Output = GF;
-                        fn sub(self, rhs: GF) -> Self::Output {
-                            GF::new(self) - rhs
-                        }
-                    }
-                    impl Mul<GF> for $ty {
-                        type Output = GF;
-                        fn mul(self, rhs: GF) -> Self::Output {
-                            GF::new(self) * rhs
-                        }
-                    }
-                    impl Div<GF> for $ty {
-                        type Output = GF;
-                        fn div(self, rhs: GF) -> Self::Output {
-                            GF::new(self) / rhs
-                        }
-                    }
-                };
-            }
-
-            def_ops!(i8);
-            def_ops!(i16);
-            def_ops!(i32);
-            def_ops!(i64);
-            def_ops!(isize);
-
-            def_ops!(u8);
-            def_ops!(u16);
-            def_ops!(u32);
-            def_ops!(u64);
-            def_ops!(usize);
-
-            impl Neg for GF {
-                type Output = GF;
-                fn neg(self) -> Self::Output {
-                    GF::new(0) - self
-                }
-            }
-
-            impl<T: TryInto<i64>> From<T> for GF {
-                fn from(v: T) -> Self {
-                    GF::new(v)
-                }
+// FIXME: Currently, rustc does not allow partial orphan instance
+// Use trait when it will be stabilized
+macro_rules! def_ops {
+    ($ty:ty) => {
+        impl<P: Unsigned> Add<GF<P>> for $ty {
+            type Output = GF<P>;
+            fn add(self, rhs: GF<P>) -> Self::Output {
+                GF::<P>::new(self) + rhs
             }
         }
-
-        use gf::GF;
+        impl<P: Unsigned> Sub<GF<P>> for $ty {
+            type Output = GF<P>;
+            fn sub(self, rhs: GF<P>) -> Self::Output {
+                GF::<P>::new(self) - rhs
+            }
+        }
+        impl<P: Unsigned> Mul<GF<P>> for $ty {
+            type Output = GF<P>;
+            fn mul(self, rhs: GF<P>) -> Self::Output {
+                GF::<P>::new(self) * rhs
+            }
+        }
+        impl<P: Unsigned> Div<GF<P>> for $ty {
+            type Output = GF<P>;
+            fn div(self, rhs: GF<P>) -> Self::Output {
+                GF::<P>::new(self) / rhs
+            }
+        }
     };
 }
 
-#[test]
-fn test_gf() {
-    def_gf!(1000000007);
+def_ops!(i8);
+def_ops!(i16);
+def_ops!(i32);
+def_ops!(i64);
+def_ops!(isize);
 
-    // operator test
-    let _ = GF::new(0);
-    let x: GF = 0.into();
-    let x = -x;
+def_ops!(u8);
+def_ops!(u16);
+def_ops!(u32);
+def_ops!(u64);
+def_ops!(usize);
 
-    let x = 1 + x;
-    let x = x + 1;
-    let mut x = x + x;
-    x += 1;
+impl<P: Unsigned> Neg for GF<P> {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self::new(0) - self
+    }
+}
 
-    let x = 1 - x;
-    let x = x - 1;
-    let mut x = x - x;
-    x -= 1;
+impl<P: Unsigned, T: TryInto<i64>> From<T> for GF<P> {
+    fn from(v: T) -> Self {
+        Self::new(v)
+    }
+}
 
-    let x = 1 * x;
-    let x = x * 1;
-    let mut x = x * x;
-    x *= 1;
+#[cfg(test)]
+mod test {
+    use typenum::Unsigned;
+    use typenum_promote::promote;
 
-    let x = 1 / x;
-    let x = x / 1;
-    let mut x = x / x;
-    x /= 1;
+    #[test]
+    fn test_gf() {
+        type P = promote!(1000000007);
+        type GF = super::GF<P>;
 
-    // basic tests
-    let x: GF = 12345678.into();
-    let y: GF = 87654321.into();
-    assert_eq!(y * x * x.recip(), y);
+        // operator test
+        let _ = GF::new(0);
+        let x: GF = 0.into();
+        let x = -x;
 
-    assert_eq!(GF::new(2).pow(50).0, (1 << 50) % gf::P)
+        let x = 1 + x;
+        let x = x + 1;
+        let mut x = x + x;
+        x += 1;
+
+        let x = 1 - x;
+        let x = x - 1;
+        let mut x = x - x;
+        x -= 1;
+
+        let x = 1 * x;
+        let x = x * 1;
+        let mut x = x * x;
+        x *= 1;
+
+        let x = 1 / x;
+        let x = x / 1;
+        let mut x = x / x;
+        x /= 1;
+
+        // basic tests
+        let x: GF = 12345678.into();
+        let y: GF = 87654321.into();
+        assert_eq!(y * x * x.recip(), y);
+
+        assert_eq!(GF::new(2).pow(50).0, (1 << 50) % <P as Unsigned>::to_u64())
+    }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -5,6 +5,7 @@ pub use std::{i128, i16, i32, i64, i8, isize, u128, u16, u32, u64, u8, usize};
 
 pub use itertools::Itertools;
 pub use memoise::memoise;
+pub use typenum_promote::promote;
 
 // input! macro
 pub use proconio::input;
@@ -19,12 +20,12 @@ pub use num::{abs, clamp, BigInt, BigRational, BigUint, Num, Rational};
 pub use comprehension::*;
 
 // macros
-pub use crate::def_gf;
 pub use crate::echo;
 
 // re-exports
 pub use crate::binary_search::{binary_search, lower_bound, upper_bound};
 pub use crate::bits::{power_bitset, SmallBitSet};
+pub use crate::gf::GF;
 pub use crate::inf::{MaybeInf, MaybeInf::*};
 pub use crate::ix::{Board, Ix2};
 pub use crate::util::{tf, yn, Echo, Mat, MatS};
